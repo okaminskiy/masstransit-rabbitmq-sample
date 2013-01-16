a4c0923fce1b136a5c5c574d0f740c3241b3c992
123 <olkorule333@gmail.com>
2013-01-16 17:47:54 +0200

    Commit 2

---
+++ WebSiteNew/Controllers/PublishController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Domain.Messages;
+using MassTransit;
+using WebSiteNew.Models;
+
+namespace WebSiteNew.Controllers
+{
+    public class PublishController : Controller
+    {
+        //
+        // GET: /Publish/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Index(SimpleMessage model)
+        {
+            Bus.Instance.Publish(new ParseCvMessage { S3Key = model.Message });
+            return View();
+        }
+
+    }
+}
