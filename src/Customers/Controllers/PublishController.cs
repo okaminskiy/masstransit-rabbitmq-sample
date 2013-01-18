b5eecda557faa88d601c5b79f49d1d72b81037a9
123 <olkorule333@gmail.com>
2013-01-18 13:05:20 +0200

    Add Customer Project

---
+++ src/Customers/Controllers/PublishController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using Domain.Messages;
+using MassTransit;
+
+namespace Customers.Controllers
+{
+    public class PublishController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Send(string message)
+        {
+            Bus.Instance.Publish(new ActivateCustomerCommand { S3Key = message });
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ src/Customers/Global.asax.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using Customers.App_Start;
+using MassTransit;
+using Topshelf;
+using Domain.Messages;
+
+namespace Customers
+{
+	// Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+	// visit http://go.microsoft.com/?LinkId=9394801
+	public class MvcApplication : System.Web.HttpApplication
+	{
+	    public static IWindsorContainer Container;
+		
+        protected void Application_Start()
+		{
+			AreaRegistration.RegisterAllAreas();
+
+			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+			ConfigureBus();
+		}
+
+		void ConfigureBus()
+		{
+		    Container = new WindsorContainer();
+            Container.Register(AllTypes.FromThisAssembly().BasedOn<IConsumer>());
+			Bus.Initialize(sbc =>
+			{
+				sbc.UseRabbitMq();
+				sbc.UseRabbitMqRouting();
+				// this should be different from other endpoints in the project
+				sbc.ReceiveFrom("rabbitmq://localhost/sample.web");
+                sbc.Subscribe(sb => 
+                    sb.LoadFrom(Container));
+                
+			});
+
+
+		}
+
+        public class AuthorizeCustomerSubscribe: Consumes<CustomerCreated>.All
+        {
+
+            public void Consume(CustomerCreated message)
+            {
+                Bus.Instance.Publish(new AuthorizeCustomer { Email = message.Email, CustomerId = message.CustomerId });
+            }
+        }
+    }       
+
+}
